c63c09f189744e658e242d17864fda1df1d72b9d
amrufathy <amru_fathy@yahoo.com>
2015-11-10 10:35:38 +0200

    Add Quiz files

---
+++ src/quiz/Quiz.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quiz;
+
+import java.util.ArrayList;
+import question.Question;
+
+/**
+ *
+ * @author amr
+ */
+public class Quiz {
+
+    private ArrayList<Question> questions;
+    private String name;
+    private int score;
+
+    public Quiz(String name) {
+        this.name = name;
+        this.score = 0;
+        questions = new ArrayList<>();
+    }
+
+    public ArrayList<Question> getQuestions() {
+        return questions;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void incrementScore() {
+        this.score++;
+    }
+
+    public void addQuestion(Question q) {
+        questions.add(q);
+    }
+
+}
